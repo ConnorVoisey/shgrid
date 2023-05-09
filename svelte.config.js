7b40915b8b4c7be9fc21f6a4a386fc712f454082
ConnorVoisey <connor.voisey@gmail.com>
2023-05-09 09:57:01 +0100

    fix: tsconfig now uses the index file

--- svelte.config.js
+++ svelte.config.js
@@ -11,15 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-
-		typescript: {
-			config: (config) => {
-                console.log(config.exclude)
-                config.exclude.push('../src/lib/index.js');
-                return config
-			}
-		}
+		adapter: adapter()
 	}
 };
 
