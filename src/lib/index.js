c06b92e93dd15f1ec660dba3fa1253048ed5a783
ConnorVoisey <connor.voisey@gmail.com>
2023-05-09 09:33:22 +0100

    fix: added compiled files to ignore and added fixed tsconfig

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
 // Reexport your entry components here
 export { default as Shgrid } from './shgrid.svelte';
-export { BaseGridBuilder } from './js/BaseGridBuilder.js';
-export { ServerGridBuilder } from './js/ServerGridBuilder.js';
+export { BaseGridBuilder } from './js/BaseGridBuilder';
+export { ServerGridBuilder } from './js/ServerGridBuilder';
--- vanilla/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { default as Shgrid } from "./shgrid.svelte";
