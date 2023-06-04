bac0c0a618bf2cebe1d41360921789145d0ef387
ConnorVoisey <connor.voisey@gmail.com>
2023-06-04 18:23:15 +0100

    feat: options backdrop overlay

--- src/lib/shgrid.svelte
+++ src/lib/shgrid.svelte
@@ -40,126 +40,126 @@
 			</button>
 		</label>
 	</div>
-	{#if isOptionsOpen}
-		<Options {builder} />
-	{:else}
-		<div class="flex">
-			<table>
-				<thead>
-					<tr>
-						<th />
-						{#each notHiddenColumns as column}
-							<th>
-								<div class="container">
-									<label class="flex">
-										<p>{column.label}</p>
-										<button class="icon" on:click={() => builder.sortColumn(column.id)}>
-											{#if builder.sorters?.[0]?.columnId === column.id}
-												{#if builder.sorters?.[0]?.isAsc}
-													<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-														><title>sort-alphabetical-descending</title><path
-															d="M19 7H22L18 3L14 7H17V21H19M11 13V15L7.67 19H11V21H5V19L8.33 15H5V13M9 3H7C5.9 3 5 3.9 5 5V11H7V9H9V11H11V5C11 3.9 10.11 3 9 3M9 7H7V5H9Z"
-														/></svg
-													>
-												{:else}
-													<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-														><title>sort-alphabetical-ascending</title><path
-															d="M19 17H22L18 21L14 17H17V3H19M11 13V15L7.67 19H11V21H5V19L8.33 15H5V13M9 3H7C5.9 3 5 3.9 5 5V11H7V9H9V11H11V5C11 3.9 10.11 3 9 3M9 7H7V5H9Z"
-														/></svg
-													>
-												{/if}
+	<div class="table-wrapper">
+		{#if isOptionsOpen}
+			<Options {builder} />
+			<button class="overlay-background" on:click={()=>isOptionsOpen = false}/>
+		{/if}
+		<table class:in-background={isOptionsOpen}>
+			<thead>
+				<tr>
+					<th />
+					{#each notHiddenColumns as column}
+						<th>
+							<div class="container">
+								<label class="flex">
+									<p>{column.label}</p>
+									<button class="icon" on:click={() => builder.sortColumn(column.id)}>
+										{#if builder.sorters?.[0]?.columnId === column.id}
+											{#if builder.sorters?.[0]?.isAsc}
+												<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+													><title>sort-alphabetical-descending</title><path
+														d="M19 7H22L18 3L14 7H17V21H19M11 13V15L7.67 19H11V21H5V19L8.33 15H5V13M9 3H7C5.9 3 5 3.9 5 5V11H7V9H9V11H11V5C11 3.9 10.11 3 9 3M9 7H7V5H9Z"
+													/></svg
+												>
 											{:else}
 												<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-													><title>sort</title><path
-														d="M18 21L14 17H17V7H14L18 3L22 7H19V17H22M2 19V17H12V19M2 13V11H9V13M2 7V5H6V7H2Z"
+													><title>sort-alphabetical-ascending</title><path
+														d="M19 17H22L18 21L14 17H17V3H19M11 13V15L7.67 19H11V21H5V19L8.33 15H5V13M9 3H7C5.9 3 5 3.9 5 5V11H7V9H9V11H11V5C11 3.9 10.11 3 9 3M9 7H7V5H9Z"
 													/></svg
 												>
 											{/if}
-										</button>
-									</label>
-									<input
-										type="text"
-										on:input={(e) => {
-											column.filter = e.currentTarget.value;
-											builder.buildData();
-										}}
-									/>
-								</div>
-							</th>
-						{/each}
-					</tr>
-				</thead>
-				<tbody>
-					{#if builder.loading}
-						<tr>
-							<td colspan="999">
-								<Loading />
-							</td>
-						</tr>
-					{:else}
-						{#each rows as row, i}
-							<tr class:odd-darker={i % 2}>
-								<td>
-									<button class="expand-btn" on:click={() => (row.isOpen = !row.isOpen)}>
-										<div>
-											{#if row.isOpen}
-												<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-													<title>Shrink</title>
-													<path
-														d="M19.5,3.09L15,7.59V4H13V11H20V9H16.41L20.91,4.5L19.5,3.09M4,13V15H7.59L3.09,19.5L4.5,20.91L9,16.41V20H11V13H4Z"
-													/>
-												</svg>
-											{:else}
-												<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-													<title>Expand</title>
-													<path
-														d="M10,21V19H6.41L10.91,14.5L9.5,13.09L5,17.59V14H3V21H10M14.5,10.91L19,6.41V10H21V3H14V5H17.59L13.09,9.5L14.5,10.91Z"
-													/>
-												</svg>
-											{/if}</div
-										>
-									</button>
-								</td>
-								{#each notHiddenColumns as column, i}
-									<td>
-										{#if builder.rowLink === undefined && column.link === undefined}
-											<div>{@html builder.formatCell(row.data, column.id)}</div>
 										{:else}
-											<a
-												href={(() => {
-													if (column.link !== undefined) return column.link(row.data);
-													if (builder.rowLink !== undefined) return builder.rowLink(row.data);
-												})()}
-												class:hover-highlight-row={column.link === undefined}
-												class:hover-highlight-cell={column.link !== undefined}
-											>
-												{@html builder.formatCell(row.data, column.id)}</a
+											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+												><title>sort</title><path
+													d="M18 21L14 17H17V7H14L18 3L22 7H19V17H22M2 19V17H12V19M2 13V11H9V13M2 7V5H6V7H2Z"
+												/></svg
 											>
 										{/if}
-									</td>
-								{/each}
-							</tr>
+									</button>
+								</label>
+								<input
+									type="text"
+									on:input={(e) => {
+										column.filter = e.currentTarget.value;
+										builder.buildData();
+									}}
+								/>
+							</div>
+						</th>
+					{/each}
+				</tr>
+			</thead>
+			<tbody>
+				{#if builder.loading}
+					<tr>
+						<td colspan="999">
+							<Loading />
+						</td>
+					</tr>
+				{:else}
+					{#each rows as row, i}
+						<tr class:odd-darker={i % 2}>
+							<td>
+								<button class="expand-btn" on:click={() => (row.isOpen = !row.isOpen)}>
+									<div>
+										{#if row.isOpen}
+											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+												<title>Shrink</title>
+												<path
+													d="M19.5,3.09L15,7.59V4H13V11H20V9H16.41L20.91,4.5L19.5,3.09M4,13V15H7.59L3.09,19.5L4.5,20.91L9,16.41V20H11V13H4Z"
+												/>
+											</svg>
+										{:else}
+											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+												<title>Expand</title>
+												<path
+													d="M10,21V19H6.41L10.91,14.5L9.5,13.09L5,17.59V14H3V21H10M14.5,10.91L19,6.41V10H21V3H14V5H17.59L13.09,9.5L14.5,10.91Z"
+												/>
+											</svg>
+										{/if}
+									</div>
+								</button>
+							</td>
+							{#each notHiddenColumns as column, i}
+								<td>
+									{#if builder.rowLink === undefined && column.link === undefined}
+										<div>{@html builder.formatCell(row.data, column.id)}</div>
+									{:else}
+										<a
+											href={(() => {
+												if (column.link !== undefined) return column.link(row.data);
+												if (builder.rowLink !== undefined) return builder.rowLink(row.data);
+											})()}
+											class:hover-highlight-row={column.link === undefined}
+											class:hover-highlight-cell={column.link !== undefined}
+										>
+											{@html builder.formatCell(row.data, column.id)}</a
+										>
+									{/if}
+								</td>
+							{/each}
+						</tr>
 
-							{#if row.isOpen}
-								<tr class="expanded" class:odd-darker={i % 2}>
-									<td colspan="999" class="expanded">
-										<div class="wrapper">
-											{#each builder.columns as column}
-												<div class="row">
-													<p>{column.label}:</p>
-													<p>{JSON.stringify(builder.data[i][column.id])}</p>
-												</div>
-											{/each}
-										</div>
-									</td>
-								</tr>
-							{/if}
-						{/each}
-					{/if}
-				</tbody>
-			</table>
-		</div>
-	{/if}
+						{#if row.isOpen}
+							<tr class="expanded" class:odd-darker={i % 2}>
+								<td colspan="999" class="expanded">
+									<div class="wrapper">
+										{#each builder.columns as column}
+											<div class="row">
+												<p>{column.label}:</p>
+												<p>{JSON.stringify(builder.data[i][column.id])}</p>
+											</div>
+										{/each}
+									</div>
+								</td>
+							</tr>
+						{/if}
+					{/each}
+				{/if}
+			</tbody>
+		</table>
+	</div>
 
 	<Paginator {builder} />
 </div>
